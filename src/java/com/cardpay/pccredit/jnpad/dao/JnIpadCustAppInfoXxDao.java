20f789979107d2102ba9da97162c5fea0ac32736
songchengxian <411137909@qq.com>
2016-05-16 14:36:31 +0800

    add pad接口

--- src/java/com/cardpay/pccredit/jnpad/dao/JnIpadCustAppInfoXxDao.java
+++ src/java/com/cardpay/pccredit/jnpad/dao/JnIpadCustAppInfoXxDao.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.annotations.Param;
 
 import com.cardpay.pccredit.intopieces.model.IntoPieces;
 import com.cardpay.pccredit.jnpad.filter.CustomerApprovedFilter;
+import com.cardpay.pccredit.jnpad.filter.NotificationMessageFilter;
+import com.cardpay.pccredit.notification.model.NotificationMessage;
 import com.wicresoft.util.annotation.Mapper;
 
 @Mapper
@@ -18,15 +20,16 @@ public interface JnIpadCustAppInfoXxDao {
 						    		  @Param("status4") String status4);
     
     
-    
     public List<IntoPieces> findCustomerApprovedList(CustomerApprovedFilter filter);
-	
 	public int findCustomerApprovedCountList(CustomerApprovedFilter filter);
 	
 	public int updateCustomerApplicationInfo(CustomerApprovedFilter filter);
-	
 	public String calCreditAmt(CustomerApprovedFilter filter);
-	
 	public int overdueCustomerCount(CustomerApprovedFilter filter);
 	
+	public List<NotificationMessage> findNotificationMessageByFilter(NotificationMessageFilter filter);
+	public int findNotificationCountMessageByFilter(NotificationMessageFilter filter);
+	
+	public String getRewardIncentiveInformation(@Param("year") int year,@Param("month") int month,@Param("id") String id);
+	public String getReturnPrepareAmountById(@Param("year") int year,@Param("month") int month,@Param("id") String id);
 }
