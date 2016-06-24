1c4ec76d2d704bf806ff33a09e8f648a0ee03471
dosealy <sealy.dong@gmail.com>
2016-06-24 10:52:43 +0800

    update 客户查询功能修改

--- src/java/com/cardpay/pccredit/jnpad/service/JnpadCustomerSelectService.java
+++ src/java/com/cardpay/pccredit/jnpad/service/JnpadCustomerSelectService.java
@@ -1,11 +1,16 @@
 package com.cardpay.pccredit.jnpad.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cardpay.pccredit.customer.model.CustomerFirsthendBase;
 import com.cardpay.pccredit.jnpad.dao.JnpadCustomerSelectDao;
 import com.cardpay.pccredit.jnpad.model.CIPERSONBASINFO;
 import com.cardpay.pccredit.jnpad.model.CustomerInfo;
+import com.cardpay.pccredit.system.model.SystemUser;
+import com.wicresoft.jrad.base.database.dao.common.CommonDao;
 
 /**
  * 客户信息查询
@@ -18,7 +23,8 @@ public class JnpadCustomerSelectService {
 	@Autowired
 	private JnpadCustomerSelectDao jnpadCustomerSelectDao;
 	
-	
+	@Autowired
+	private CommonDao commonDao;
 	/**
 	 * 根据证件号码查询
 	 * @return
@@ -52,27 +58,33 @@ public class JnpadCustomerSelectService {
 	}
 
 
+	public CustomerFirsthendBase findCustomerFirsthendById(String customerInforId) {
+		// TODO Auto-generated method stub
+		 return commonDao.findObjectById(CustomerFirsthendBase.class,customerInforId);
+	}
+
+//按身份证号和客户经理ID
+	public CustomerInfo selectCustomerInfoByCardIdAndUserId(String cardId, String userId) {
+		// TODO Auto-generated method stub
+		return jnpadCustomerSelectDao.selectCustomerInforByCardIdAndUserId(cardId, userId);
+	}
+
+
+	public List<CustomerInfo> selectCustomerInfo(String cardId,String chineseName, String userId) {
+		// TODO Auto-generated method stub
+		return jnpadCustomerSelectDao.selectCustomerInfo(cardId,chineseName, userId);
+	}
+
+
+	public String selectManagerNameById(String gId) {
+		// TODO Auto-generated method stub
+		SystemUser user = commonDao.findObjectById(SystemUser.class, gId);
+		
+		
+		return user.getDisplayName();
+	}
 
 
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
