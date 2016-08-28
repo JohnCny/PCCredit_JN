65c91d96cddf36814e5c402d2307fff53cdeae53
dosealy <sealy.dong@gmail.com>
2016-08-28 11:50:28 +0800

    update pad端图片调查模板在线预览

---
+++ src/java/com/cardpay/pccredit/jnpad/dao/JnpadImageBrowseDao.java
@@ -0,0 +1,15 @@
+package com.cardpay.pccredit.jnpad.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import com.cardpay.pccredit.intopieces.web.LocalImageForm;
+import com.wicresoft.util.annotation.Mapper;
+
+@Mapper
+public interface JnpadImageBrowseDao {
+
+	public List<LocalImageForm> findLocalImage(@Param("customerId") String id);
+
+	
+	
+}
