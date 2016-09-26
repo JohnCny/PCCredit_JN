79b03d04e443194f8edc33aca9c636a726308964
songchengxian <411137909@qq.com>
2016-09-26 11:28:27 +0800

    update 贷后管理查询筛选 审贷结论增加申请时间查询

--- src/java/com/cardpay/pccredit/postLoan/web/Loan_TY_JJB_Controller.java
+++ src/java/com/cardpay/pccredit/postLoan/web/Loan_TY_JJB_Controller.java
@@ -149,6 +149,7 @@ public class Loan_TY_JJB_Controller extends BaseController {
 			filter.setUserId(userId);
 			mv.addObject("type", "1");//客户经理
 		}
+		filter.setStatus("end");
 		result = intoPiecesService.findintoPiecesByFilter(filter);
 		JRadPagedQueryResult<IntoPieces> pagedResult = new JRadPagedQueryResult<IntoPieces>(filter, result);
 		mv.addObject(PAGED_RESULT, pagedResult);
