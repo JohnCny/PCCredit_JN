cc5d20340505db78c2444a60a1db9153894d869f
dosealy <sealy.dong@gmail.com>
2017-01-06 09:47:54 +0800

    Revert "update"

--- src/java/com/cardpay/pccredit/ipad/constant/IpadConstant.java
+++ src/java/com/cardpay/pccredit/ipad/constant/IpadConstant.java
@@ -5,7 +5,7 @@ public class IpadConstant {
 	public static final String FAIL = "fail";
 	public static final String LOGINNOTNULL = "账号或密码为空";
 	public static final String LOGINSUCCESS = "登陆成功";
-	public static final String LOGINFAIL = "密码错误";
+	public static final String LOGINFAIL = "登陆失败";
 	public static final String CREATESUCCESS = "添加成功";
 	public static final String CREATEFAIL = "添加失败"; 
 	public static final String SYSTEMERROR = "系统异常";
