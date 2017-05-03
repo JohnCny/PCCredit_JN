9d1fccf61e61e3aac5dac05215c2749a78ac2af0
tanwh <tan_w_h@sina.com>
2017-05-03 13:44:03 +0800

    [add]聊天记录表

---
+++ src/java/com/cardpay/pccredit/intopieces/service/ChatMessageService.java
@@ -0,0 +1,86 @@
+package com.cardpay.pccredit.intopieces.service;
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cardpay.pccredit.common.UploadFileTool;
+import com.cardpay.pccredit.customer.model.CustomerCareersInformation;
+import com.cardpay.pccredit.customer.model.CustomerInfor;
+import com.cardpay.pccredit.customer.service.CustomerInforService;
+import com.cardpay.pccredit.divisional.constant.DivisionalProgressEnum;
+import com.cardpay.pccredit.divisional.constant.DivisionalTypeEnum;
+import com.cardpay.pccredit.divisional.service.DivisionalService;
+import com.cardpay.pccredit.intopieces.constant.Constant;
+import com.cardpay.pccredit.intopieces.constant.IntoPiecesException;
+import com.cardpay.pccredit.intopieces.dao.ChatMessageDao;
+import com.cardpay.pccredit.intopieces.dao.IntoPiecesDao;
+import com.cardpay.pccredit.intopieces.dao.comdao.IntoPiecesComdao;
+import com.cardpay.pccredit.intopieces.filter.IntoPiecesFilter;
+import com.cardpay.pccredit.intopieces.filter.MakeCardFilter;
+import com.cardpay.pccredit.intopieces.model.AppManagerAuditLog;
+import com.cardpay.pccredit.intopieces.model.AppManagerAuditLogForm;
+import com.cardpay.pccredit.intopieces.model.ApplicationDataImport;
+import com.cardpay.pccredit.intopieces.model.BasicCustomerInformationS;
+import com.cardpay.pccredit.intopieces.model.ChatMessage;
+import com.cardpay.pccredit.intopieces.model.CustomerAccountData;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationCom;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationContact;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationGuarantor;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationInfo;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationOther;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationProcess;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationProcessForm;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationRecom;
+import com.cardpay.pccredit.intopieces.model.CustomerCareersInformationS;
+import com.cardpay.pccredit.intopieces.model.CustomerCreditInfo;
+import com.cardpay.pccredit.intopieces.model.EvaResult;
+import com.cardpay.pccredit.intopieces.model.IntoPieces;
+import com.cardpay.pccredit.intopieces.model.MakeCard;
+import com.cardpay.pccredit.intopieces.model.QzApplnAttachmentDetail;
+import com.cardpay.pccredit.intopieces.model.VideoAccessories;
+import com.cardpay.pccredit.intopieces.web.ApproveHistoryForm;
+import com.cardpay.pccredit.manager.model.AccountManagerParameter;
+import com.cardpay.pccredit.manager.model.ManagerBelongMap;
+import com.cardpay.pccredit.postLoan.model.MibusidataForm;
+import com.cardpay.pccredit.product.model.AddressAccessories;
+import com.cardpay.pccredit.riskControl.model.RiskCustomer;
+import com.cardpay.pccredit.system.model.SystemUser;
+import com.wicresoft.jrad.base.database.dao.common.CommonDao;
+import com.wicresoft.jrad.base.database.model.BusinessModel;
+import com.wicresoft.jrad.base.database.model.QueryResult;
+
+@Service
+public class ChatMessageService {
+
+	// TODO 路径使用相对路径，首先获得应用所在路径，之后建立上传文件目录，图片类型使用IMG，文件使用DOC
+
+	@Autowired
+	private CommonDao commonDao;
+	
+	@Autowired
+	private ChatMessageDao chatMessageDao;
+
+	
+	/* 查询进价信息 */
+	/*
+	 * TODO 1.添加注释 2.SQL写进DAO层
+	 */
+	public QueryResult<ChatMessage> findByApplicationId(String applicationId) {
+		List<ChatMessage> msglist = chatMessageDao.findByApplicationId(applicationId);
+		int size = chatMessageDao.findCountByApplicationId(applicationId);
+		QueryResult<ChatMessage> queryResult = new QueryResult<ChatMessage>(size, msglist);
+		return queryResult;
+	}
+}
