5d3eb0a622d1c7a8bb7cfe1d0531dfd8fbf866e6
sealy <dongxl@qkjr.com.cn>
2017-06-28 16:26:07 +0800

    update　pad图片浏览

--- src/java/com/cardpay/pccredit/jnpad/web/JnpadAddIntoPiecesController.java
+++ src/java/com/cardpay/pccredit/jnpad/web/JnpadAddIntoPiecesController.java
@@ -2,11 +2,13 @@ package com.cardpay.pccredit.jnpad.web;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.velocity.runtime.directive.Foreach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,6 +23,8 @@ import com.cardpay.pccredit.customer.filter.CustomerInforFilter;
 import com.cardpay.pccredit.customer.model.CustomerInfor;
 import com.cardpay.pccredit.customer.service.CustomerInforService;
 import com.cardpay.pccredit.intopieces.filter.AddIntoPiecesFilter;
+import com.cardpay.pccredit.intopieces.model.QzApplnAttachmentBatch;
+import com.cardpay.pccredit.intopieces.model.QzApplnAttachmentList;
 import com.cardpay.pccredit.intopieces.service.AddIntoPiecesService;
 import com.cardpay.pccredit.intopieces.service.IntoPiecesService;
 import com.cardpay.pccredit.intopieces.web.AddIntoPiecesForm;
@@ -31,6 +35,7 @@ import com.cardpay.pccredit.riskControl.model.RiskCustomer;
 import com.wicresoft.jrad.base.auth.IUser;
 import com.wicresoft.jrad.base.auth.JRadOperation;
 import com.wicresoft.jrad.base.constant.JRadConstants;
+import com.wicresoft.jrad.base.database.dao.common.CommonDao;
 import com.wicresoft.jrad.base.database.model.QueryResult;
 import com.wicresoft.jrad.base.web.JRadModelAndView;
 import com.wicresoft.jrad.base.web.result.JRadPagedQueryResult;
@@ -63,6 +68,9 @@ public class JnpadAddIntoPiecesController {
 	
 	@Autowired
 	private UserLogService userLogService;
+	
+	@Autowired
+	private CommonDao commonDao;
 	//选择客户
 	@ResponseBody
 	@RequestMapping(value = "/ipad/addIntoPieces/browseCustomer.json", method = { RequestMethod.GET })
@@ -98,6 +106,7 @@ public class JnpadAddIntoPiecesController {
 				String applicationId = request.getParameter("applicationId");
 				jnpadaddIntoPiecesService.importImage(file,productId,customerId,applicationId,fileName);
 				map.put(JRadConstants.SUCCESS, true);
+//				map.put("successToOther", false);
 				map.put(JRadConstants.MESSAGE, CustomerInforConstant.IMPORTSUCCESS);
 				JSONObject obj = JSONObject.fromObject(map);
 				response.getWriter().print(obj.toString());
@@ -198,7 +207,7 @@ public class JnpadAddIntoPiecesController {
 				OperationLog ol = new OperationLog();
 				ol.setUser_id(loginId);
 			    ol.setUser_login(displayName);
-			    ol.setModule("进件新增");
+			    ol.setModule("PAD进件新增");
 			    ol.setOperation_result("SUCCESS");
 			    ol.setOperation_name("ADD");
 			    ol.setIp_address(request.getRemoteAddr());
@@ -211,7 +220,7 @@ public class JnpadAddIntoPiecesController {
 				OperationLog ol = new OperationLog();
 				ol.setUser_id(loginId);
 			    ol.setUser_login(displayName);
-			    ol.setModule("进件新增");
+			    ol.setModule("PAD进件新增");
 			    ol.setOperation_result("FAIL");
 			    ol.setOperation_name("ADD");
 			    ol.setIp_address(request.getRemoteAddr());
@@ -225,4 +234,94 @@ public class JnpadAddIntoPiecesController {
 			JSONObject json = JSONObject.fromObject(map, jsonConfig);
 			return json.toString();
 		}
+		
+		//上传影像资料到［其他］分支
+				@ResponseBody
+				@RequestMapping(value = "/ipad/addIntopieces/imageImportToOther.json")
+				public Map<String, Object> imageImportToOther(@RequestParam(value = "file", required = false) MultipartFile file,HttpServletRequest request,HttpServletResponse response) throws Exception {        
+					response.setContentType("text/html;charset=utf-8");
+					Map<String, Object> map = new HashMap<String, Object>();
+					String fileName =request.getParameter("fileName");
+					String custId = request.getParameter("customerId");
+					String appId = request.getParameter("applicationId");
+					String loginId = request.getParameter("loginId");
+					String displayName = request.getParameter("displayName");
+					try {
+						if(file==null||file.isEmpty()){
+							map.put(JRadConstants.SUCCESS, false);
+							map.put(JRadConstants.MESSAGE, CustomerInforConstant.IMPORTEMPTY);
+							JSONObject obj = JSONObject.fromObject(map);
+							response.getWriter().print(obj.toString());
+						}
+						//
+						QzApplnAttachmentList att = addIntoPiecesService.findAttachmentListByAppId(appId);
+						if(att==null){
+							QzApplnAttachmentList attlist = new QzApplnAttachmentList();
+							attlist.setApplicationId(appId);
+							attlist.setCustomerId(custId);
+							attlist.setChkValue("19");
+							commonDao.insertObject(attlist);
+						}
+						//查找sunds_ocx信息
+						List<QzApplnAttachmentBatch> batch_ls = addIntoPiecesService.findAttachmentBatchByAppId(appId);
+						//如果batch_ls为空 说明这是以前录得件 根据chk_value增加batch记录
+						if(batch_ls == null || batch_ls.size() == 0){
+							addIntoPiecesService.addBatchInfo(appId,custId);
+							batch_ls = addIntoPiecesService.findAttachmentBatchByAppId(appId);
+						}
+						String batch_id=null;
+						 for (QzApplnAttachmentBatch qzApplnAttachmentBatch : batch_ls) {
+							
+							 if(qzApplnAttachmentBatch.getName().equals("其他")||qzApplnAttachmentBatch.getName()=="其他"){
+								 batch_id=qzApplnAttachmentBatch.getId();
+							 }
+						}
+						if(batch_id==null){
+							imageImport(file,request,response);
+							//日志记录
+							OperationLog ol = new OperationLog();
+							ol.setUser_id(loginId);
+						    ol.setUser_login(displayName);
+						    ol.setModule("PAD上传影像资料");
+						    ol.setOperation_result("未查找到[其他]id,导入征信报告处");
+						    ol.setOperation_name("ADD");
+						    ol.setIp_address(request.getRemoteAddr());
+							userLogService.addUserLog(ol);
+							return null;
+						}else{
+						//更新batch
+							jnpadaddIntoPiecesService.browse_folder(file,batch_id,fileName);
+							jnpadaddIntoPiecesService.browse_folder_complete(batch_id,request);
+							//日志记录
+							OperationLog ol = new OperationLog();
+							ol.setUser_id(loginId);
+						    ol.setUser_login(displayName);
+						    ol.setModule("PAD上传影像资料");
+						    ol.setOperation_result("success");
+						    ol.setOperation_name("ADD");
+						    ol.setIp_address(request.getRemoteAddr());
+							userLogService.addUserLog(ol);
+						map.put(JRadConstants.SUCCESS, true);
+						map.put(JRadConstants.MESSAGE, CustomerInforConstant.IMPORTSUCCESS);
+						JSONObject obj = JSONObject.fromObject(map);
+						response.getWriter().print(obj.toString());
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+						map.put(JRadConstants.SUCCESS, false);
+						map.put(JRadConstants.MESSAGE, "上传失败:"+e.getMessage());
+						//日志记录
+						OperationLog ol = new OperationLog();
+						ol.setUser_id(loginId);
+					    ol.setUser_login(displayName);
+					    ol.setModule("PAD上传影像资料");
+					    ol.setOperation_result("fail");
+					    ol.setOperation_name("ADD");
+					    ol.setIp_address(request.getRemoteAddr());
+						userLogService.addUserLog(ol);
+						JSONObject obj = JSONObject.fromObject(map);
+						response.getWriter().print(obj.toString());
+					}
+					return null;
+				}
 }
