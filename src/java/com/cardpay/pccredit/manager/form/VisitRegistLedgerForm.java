28e7c79636dd8cb299937df87cfd3d7c69947ccf
songchengxian <411137909@qq.com>
2017-06-30 17:54:17 +0800

    update  拜访台账登记提交

---
+++ src/java/com/cardpay/pccredit/manager/form/VisitRegistLedgerForm.java
@@ -0,0 +1,131 @@
+package com.cardpay.pccredit.manager.form;
+
+import com.wicresoft.jrad.base.web.form.BaseForm;
+
+/**
+ * @author songchen 
+ */
+public class VisitRegistLedgerForm extends  BaseForm{
+	
+	 private String  id;                
+	 private String  visitDate;        
+	 private String  merchantName;     
+	 private String  customerName;     
+	 private String  contactInfor;     
+	 private String  sex;               
+	 private String  registence;        
+	 private String  realEstate;       
+	 private String  scopeOfBusiness; 
+	 private String  scopeOfAddress;  
+	 private String  fundDemand;       
+	 private String  fundDemandDate;  
+	 private String  financeExp;       
+	 private String  visitManager;     
+	 private String  organ;             
+	 private String  remark;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getVisitDate() {
+		return visitDate;
+	}
+	public void setVisitDate(String visitDate) {
+		this.visitDate = visitDate;
+	}
+	public String getMerchantName() {
+		return merchantName;
+	}
+	public void setMerchantName(String merchantName) {
+		this.merchantName = merchantName;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getContactInfor() {
+		return contactInfor;
+	}
+	public void setContactInfor(String contactInfor) {
+		this.contactInfor = contactInfor;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getRegistence() {
+		return registence;
+	}
+	public void setRegistence(String registence) {
+		this.registence = registence;
+	}
+	public String getRealEstate() {
+		return realEstate;
+	}
+	public void setRealEstate(String realEstate) {
+		this.realEstate = realEstate;
+	}
+	public String getScopeOfBusiness() {
+		return scopeOfBusiness;
+	}
+	public void setScopeOfBusiness(String scopeOfBusiness) {
+		this.scopeOfBusiness = scopeOfBusiness;
+	}
+	public String getScopeOfAddress() {
+		return scopeOfAddress;
+	}
+	public void setScopeOfAddress(String scopeOfAddress) {
+		this.scopeOfAddress = scopeOfAddress;
+	}
+	public String getFundDemand() {
+		return fundDemand;
+	}
+	public void setFundDemand(String fundDemand) {
+		this.fundDemand = fundDemand;
+	}
+	public String getFundDemandDate() {
+		return fundDemandDate;
+	}
+	public void setFundDemandDate(String fundDemandDate) {
+		this.fundDemandDate = fundDemandDate;
+	}
+	public String getFinanceExp() {
+		return financeExp;
+	}
+	public void setFinanceExp(String financeExp) {
+		this.financeExp = financeExp;
+	}
+	public String getVisitManager() {
+		return visitManager;
+	}
+	public void setVisitManager(String visitManager) {
+		this.visitManager = visitManager;
+	}
+	public String getOrgan() {
+		return organ;
+	}
+	public void setOrgan(String organ) {
+		this.organ = organ;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	 
+	 
+	
+	
+	 
+	 
+	 
+	
+
+}
